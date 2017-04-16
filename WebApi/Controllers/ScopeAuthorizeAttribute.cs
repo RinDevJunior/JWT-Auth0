8136b65f25a35149e3eacd3171452cb4bcf94826
rinnv <rinnv.spkt@gmail.com>
2017-04-16 09:09:49 +0700

    init

---
+++ WebApi/Controllers/ScopeAuthorizeAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace WebApi.Controllers
+{
+    public class ScopeAuthorizeAttribute : AuthorizeAttribute
+    {
+        private readonly string scope;
+
+        public ScopeAuthorizeAttribute(string scope)
+        {
+            this.scope = scope;
+        }
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            base.OnAuthorization(actionContext);
+            var x = actionContext.ControllerContext;
+            var y = x.RequestContext;
+            var z = y.Principal;
+
+            ClaimsPrincipal principal = z as ClaimsPrincipal;
+            if (principal != null && principal.HasClaim(c => c.Type == "scope"))
+            {
+                // Split the scopes string into an array
+                var scopes = principal.Claims.FirstOrDefault(c => c.Type == "scope")?.Value.Split(' ');
+
+                // Succeed if the scope array contains the required scope
+                if (scopes.Any(s => s == scope))
+                    return;
+            }
+
+            HandleUnauthorizedRequest(actionContext);
+        }
+    }
+}
---
+++ WebApi/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System.Configuration;
+using System.IdentityModel.Tokens;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.ActiveDirectory;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.Owin.Security.Jwt;
+using Owin;
+using System.Linq;
+using System.Text;
+using Auth0.Owin;
+using Microsoft.Owin.Security.OAuth;
+using AuthenticationMode = Microsoft.Owin.Security.AuthenticationMode;
+
+[assembly: OwinStartup(typeof(WebApi.Startup))]
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var domain = $"https://{ConfigurationManager.AppSettings["Auth0Domain"]}/";
+            var apiIdentifier = ConfigurationManager.AppSettings["Auth0ApiIdentifier"];
+
+            var keyResolver = new OpenIdConnectSigningKeyResolver(domain);
+            app.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = AuthenticationMode.Active,
+                    TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidAudience = apiIdentifier,
+                        ValidIssuer = domain,
+                        IssuerSigningKeyResolver = (token, securityToken, identifier, parameters) => keyResolver.GetSigningKey(identifier)
+                    }
+                });
+
+
+            //var issuer = $"https://{ConfigurationManager.AppSettings["Auth0Domain"]}/";
+            //var audience = ConfigurationManager.AppSettings["Auth0ApiIdentifier"];
+            //var secret = Encoding.UTF8.GetBytes(ConfigurationManager.AppSettings["Auth0ApiSecret"]);
+
+            //// Api controllers with an [Authorize] attribute will be validated with JWT
+            //app.UseJwtBearerAuthentication(
+            //    new JwtBearerAuthenticationOptions
+            //    {
+            //        AuthenticationMode = AuthenticationMode.Active,
+            //        AllowedAudiences = new[] { audience },
+            //        IssuerSecurityTokenProviders = new IIssuerSecurityTokenProvider[]
+            //        {
+            //            new SymmetricKeyIssuerSecurityTokenProvider(issuer, secret)
+            //        }
+            //    });
+
+
+
+            // Configure Web API
+            WebApiConfig.Configure(app);
+        }
+    }
+}
